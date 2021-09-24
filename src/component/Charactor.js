b2baad0be9c909b791f995886abfa66be6d9a661
alskfl222 <alskfl222@kakao.com>
2021-09-24 22:10:47 +0900

    #2 css 수정

--- src/component/Charactor.js
+++ src/component/Charactor.js
@@ -4,11 +4,11 @@ function Charactor ({char, type, date}) {
     const imgsrc = `http://192.168.50.130:5000/static/char/${date}_${char}.png`
 
     return (
-        <div className={`char-box ${type}`} data-value={`${char}`}>
+        <div className={`char-box`} data-value={`${char}`}>
             <div className="char-box-image" data-value={`${char}`}>
                 <img src={imgsrc} alt={char} data-value={`${char}`}/>
             </div>
-            <div className="char-name" data-value={`${char}`}>
+            <div className={`char-name ${type}`} data-value={`${char}`}>
                 {char}
             </div>
         </div>
